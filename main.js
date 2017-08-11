b2fca19629506abb0d9197506d8303568449cf8f
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-11 16:09:56 -0400

    W4 Project: iTunes - Fighting with JS. Started on CSS

--- main.js
+++ main.js
@@ -5,29 +5,26 @@
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
 
-//=============== #1 ELEMENTS =======================//
+//=============== ELEMENTS =======================//
 const results = document.querySelector('.results');
 const searchForm = document.querySelectorAll('form')[0];
 const baseUrl = 'https://itunes.apple.com/';
-
-//=============== #2 SUBMIT EVENT =======================//
-searchForm.onsubmit = function () {
-  event.preventDefault();
-
-
-}; //end onsumbit fn
-
-//=============== #3 FETCH DATA =======================//
-function search_iTunes () {
- return fetch(`${baseUrl}search?term=jack+johnson`)
+// let tempSearch = 'jack johnson';
+// let tempResults = [];
+const loopArray = [];
+//=============== FETCH DATA fn =======================//
+function search_iTunes (item) {
+ return fetch(`${baseUrl}search?term=${item}`)
    .then(function(response) {
      return response.json();
+     loopArray = results.results;
+     return loopArray;
  })
 }
-console.log(search_iTunes());
-
-//=============== #4 CREATE CARDS RESULTS =======================//
-function makeArtistHTML (artist) {
+console.log("search_iTunes ==> " + search_iTunes());
+console.log("CL for const loopArray ==> " + loopArray);
+//=============== CREATE 1 SONG fn =======================//
+function make1songHTML (song) {
   return `
   <section class="song">
     <a href="${results.previewUrl}">
@@ -38,6 +35,37 @@ function makeArtistHTML (artist) {
   </section>
   `
 }
+
+//=============== LOOP CREATE SONGS fn =======================//
+function makeSongsHTML (songs) {
+    let html = '';
+    for (let i in loopArray) {
+      html += make1songHTML(song);
+    }
+  return html;
+}
+console.log("makeSongsHTML ==> " + makeSongsHTML());
+//=============== LOOP CREATE SONGS fn =======================//
+function addSongsToDom (songsData) {
+  results.innerHTML = makeSongsHTML(songsData);
+}
+
+//=============== SUBMIT EVENT CALL fn's CREATE PAGE  =======================//
+searchForm.onsubmit = function (event) {
+  event.preventDefault();
+
+  const searchWords = event.target.querySelector('input[name="searchTerm"]').value
+    console.log("const searchWords in final call ==> " + searchWords);
+
+  search_iTunes(searchWords).then(function(results){
+    addSongsToDom(results);
+  })
+
+}; //end onsumbit fn
+
+
+
+
 
 
 // ---------------- fin ---------------------//
