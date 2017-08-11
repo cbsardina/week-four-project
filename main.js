b7c1c95682da9430bcb37aec199b8c241eb1d9c7
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-11 12:27:17 -0400

    W4 Project: iTunes - #1-3 done. Fetch pulls specified results for jack johnson in array of objects

--- main.js
+++ main.js
@@ -8,6 +8,30 @@
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
 
+//=============== #1 ELEMENTS =======================//
 const results = document.querySelector('.results');
 const searchForm = document.querySelectorAll('form')[0];
-const baseUrl = '';
+const baseUrl = 'https://itunes.apple.com/';
+
+//=============== #2 SUBMIT EVENT =======================//
+searchForm.onsubmit = function () {
+
+
+}; //end onsumbit fn
+
+//=============== #3 FETCH DATA =======================//
+let search_iTunes = function(inputWords) {
+ return fetch(`${baseUrl}search?term=jack+johnson`)
+   .then(function(response) {
+     return response.json();
+       // console.log(data);
+ })
+}
+console.log(search_iTunes());
+
+//=============== #4 APPEND RESULTS =======================//
+
+
+
+
+// ---------------- fin ---------------------//
